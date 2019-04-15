c780ee1c35e95ce82582f9c85cee04441e1643bb
PrzemekGdula <przemo.g12@wp.pl>
2019-04-15 14:18:45 +0200

    Dispatching messages from component state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React from 'react';
 
-import { sendMessage } from './store'
+import './store'
+import { sendActionCreator } from './state/messages'
 
 class App extends React.Component {
+  state = {
+    messages: []
+  }
+
+  prepareMessage = () => {
+    const receiver = prompt('Wpisz odbiorce')
+    const text = prompt('Wpisz tekst wiadomosci')
+
+    const message = sendActionCreator(receiver, text)
+
+    this.setState({
+      messages: this.state.messages.concat(message)
+    })
+  }
+
+
   render() {
     return (
       <div>
         <button
-          onClick={() => sendMessage(
-            prompt(),
-            prompt()
-          )}
+          onClick={this.prepareMessage}
+        >
+          PREPARE MESSAGE
+        </button>
+        <button
+          onClick={this.sendMessage}
         >
           SEND MESSAGE
         </button>
+
+        <div>
+          {
+            this.state.messages.map(
+              (message, i) => (
+                <div
+                  key={i + message.text}
+                >
+                  {message.receiver} | {message.text}
+                </div>
+              )
+            )
+          }
+
+
+        </div>
       </div>
     )
   }
