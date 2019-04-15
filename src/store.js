03f2bd69901ae4b4b60a13c87134acb30bcf0df6
PrzemekGdula <przemo.g12@wp.pl>
2019-04-15 11:28:00 +0200

    INC and DEC action added

--- src/store.js
+++ src/store.js
@@ -1,15 +1,40 @@
 import { createStore } from 'redux'
 
-const reducer = (state, action) => {
+const initialState = {
+    number: 0,
+}
+
+const reducer = (state = initialState, action) => {
     console.log(state, action)
 
-    return {
-        number: 0,
+    if (action.type === 'INC') {
+        return {
+            ...state,
+            number: state.number + 1
+        }
     }
+    if (action.type === 'DEC') {
+        return {
+            ...state,
+            number: state.number -1
+        }
+    }
+    return state
 }
+
 
 export const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+const incAction = {
+    type: 'INC'
+}
+
+const decAction = {
+    type: 'DEC'
+}
+
+store.dispatch(incAction)
+store.dispatch(decAction)
